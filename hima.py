698cca2d370273880ddc250e5bf53051d9f63fdd
Himaht <76118023+Himaht@users.noreply.github.com>
2022-06-13 17:50:44 +0100

    init

---
+++ hima.py
@@ -0,0 +1,52 @@
+#import math
+hima = 10 // 3
+#/ -- float division, with decimal
+#// -- integer division (math.floor) without decimal
+
+#print(hima)
+age = 864438
+hak = 10 
+
+#print(age % hak)
+
+import math
+hima = math.pow(2, 10)
+hima2 = math.floor(hima)
+#print(hima2) 
+
+#print(round(20/3))
+
+msg = "Hello\nHey" #new line-- making hey go on a new line
+#print(msg) 
+
+ki = 'he then said "hey"' #can insert a quote this way
+yus = "he actually said \"hey\"" # can also insert a quote this way
+#print(yus)
+
+msg = "My name is Himaht,"
+msg2 = "I am a Pisces"
+#print(msg, msg2)  concanetion --putting together two strings to create one argument
+
+#print(msg + "..." + msg2) -- can be done this way too or to make it more simple, the below
+
+new_msg = msg + "..." + msg2
+#print(new_msg) 
+
+mesg = ("Hello, why are you stalking me?..." #this still won't have it on a new line
+"Is everything ok?")
+
+mesg2 = ("Hello, why are you stalking me?...\nIs everything ok?")# this works for a new line
+
+#print(mesg2)
+
+poem = """So a little about me?
+I am i my early 20s as at 2022.
+I stated the year above because, who knows which year any of you is going to see this codes/repo
+I have just a BSc in Information Technology at the moment.
+At the moment because, i will be going for my masters, then my phd or maybe a second masters who knows lol.
+I am a mental health activist as well.
+A little advise, Don't stress about anything in life, it will either workout perfectly well or the other option will be far better than what you expected.
+I love you and never settle for less."""
+#To create multiple lines with each on a new line, use the triple quote """
+ 
+print(poem)
